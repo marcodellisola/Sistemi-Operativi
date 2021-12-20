80c95724ee0cca1bfaf8a80ff83ab14640dcf2f7
marcodellisola <69073588+marcodellisola@users.noreply.github.com>
2021-12-20 14:45:58 +0100

    Add files via upload

---
+++ My test/TERZA-PROVA-MARCO-DELL'ISOLA-N46005204/header.h
@@ -0,0 +1,35 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+
+
+struct monitor {
+
+	int molo;
+	int id_nave;
+
+	/* TODO: Aggiungere variabili per la sincronizzazione */
+	pthread_mutex_t mutex;
+        pthread_cond_t ok_scritt_cv;
+        pthread_cond_t ok_lett_cv;
+
+        int num_scrittori;
+        int num_lettori;
+        int num_lettori_wait;
+        int num_scrittori_wait;
+};
+
+void inizializza(struct monitor * m);
+void rimuovi (struct monitor * m);
+void scrivi_molo(struct monitor * m, int molo);
+int leggi_molo(struct monitor * m);
+
+
+
+#endif
