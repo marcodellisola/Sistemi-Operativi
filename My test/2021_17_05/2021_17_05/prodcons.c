d8409ac947b0923b78ad757fe50fbf8366c974f8
marcodellisola <69073588+marcodellisola@users.noreply.github.com>
2022-01-28 12:59:40 -0500

    Add files via upload

---
+++ My test/2021_17_05/2021_17_05/prodcons.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "prodcons.h"
+
+void inizializza(MonitorPC * m) {
+
+    printf("Inizializzazione monitor\n");
+
+    /* TBD: Inizializzazione */
+    m->num_liberi=4;
+    m->num_occupati_tipo1=0;
+    m->num_occupati_tipo2=0;
+
+    pthread_mutex_init(&(m->mutex),NULL);
+    pthread_cond_init(&(m->ok_prod_cv),NULL);
+    pthread_cond_init(&(m->ok_cons_cv),NULL);
+
+    for(int i=0; i<DIM; i++){
+	    m->vettore[i]=0;
+	    m->stato[i]=LIBERO;
+    }
+}
+
+
+void rimuovi(MonitorPC * m) {
+
+    printf("Rimozione monitor\n");
+
+    /* TBD: Rimozione */
+	pthread_mutex_destroy(&(m->mutex));
+	pthread_cond_destroy(&(m->ok_prod_cv));
+	pthread_cond_destroy(&(m->ok_cons_cv));
+}
+
+
+void produci_tipo_1(MonitorPC * m, int valore) {
+
+    int index = 0;
+
+
+    /* TBD: Sincronizzazione, e selezione dell'indice su cui produrre */
+    pthread_mutex_lock(&(m->mutex));
+    while(m->num_liberi<=0){
+	    pthread_cond_wait(&(m->ok_prod_cv),&(m->mutex));
+    }
+    if(index<=DIM && m->stato[index]!=LIBERO){
+	    index++;
+    }
+    m->stato[index]=INUSO;
+    m->num_liberi--;
+
+    printf("Produzione di tipo 1 in corso (valore=%d, index=%d)\n", valore, index);
+
+    sleep(1);
+    m->vettore[index] = valore;
+
+    m->stato[index]=OCCUPATO1;
+    m->num_occupati_tipo1++;
+    pthread_cond_signal(&(m->ok_cons_cv));
+    pthread_mutex_unlock(&(m->mutex));
+}
+
+
+void produci_tipo_2(MonitorPC * m, int valore) {
+
+    int index = 0;
+
+
+    /* TBD: Sincronizzazione, e selezione dell'indice su cui produrre */
+    pthread_mutex_lock(&(m->mutex));
+    while(m->num_liberi<=0){
+            pthread_cond_wait(&(m->ok_prod_cv),&(m->mutex));
+    }
+    if(index<=DIM && m->stato[index]!=LIBERO){
+            index++;
+    }
+    m->stato[index]=INUSO;
+    m->num_liberi--;
+
+    printf("Produzione di tipo 2 in corso (valore=%d, index=%d)\n", valore, index);
+
+    sleep(1);
+    m->vettore[index] = valore;
+
+    m->stato[index]=OCCUPATO2;
+    m->num_occupati_tipo2++;
+    pthread_cond_signal(&(m->ok_cons_cv));
+    pthread_mutex_unlock(&(m->mutex));
+}
+
+
+void consuma_tipo_1(MonitorPC * m, int * valore) {
+
+    int index = 0;
+
+
+    /* TBD: Sincronizzazione, e selezione dell'indice su cui consumare */
+    pthread_mutex_lock(&(m->mutex));
+    while(m->num_occupati_tipo1<=0){
+	    pthread_cond_wait(&(m->ok_cons_cv),&(m->mutex));
+    }
+    if(index<=DIM && m->stato[index]!=OCCUPATO1){
+	    index++;
+    }
+    m->stato[index]=INUSO;
+    m->num_occupati_tipo1--;
+
+    sleep(1);
+    *valore = m->vettore[index];
+
+    printf("Consumazione di tipo 1 (valore=%d, index=%d)\n", *valore, index);
+
+    m->stato[index]=LIBERO;
+    m->num_liberi++;
+    pthread_cond_signal(&(m->ok_prod_cv));
+    pthread_mutex_unlock(&(m->mutex));
+}
+
+
+void consuma_tipo_2(MonitorPC * m, int * valore) {
+
+    int index = 0;
+
+
+    /* TBD: Sincronizzazione, e selezione dell'indice su cui consumare */
+    pthread_mutex_lock(&(m->mutex));
+    while(m->num_occupati_tipo2<=0){
+            pthread_cond_wait(&(m->ok_cons_cv),&(m->mutex));
+    }
+    if(index<=DIM && m->stato[index]!=OCCUPATO2){
+            index++;
+    }
+    m->stato[index]=INUSO;
+    m->num_occupati_tipo2--;
+
+    sleep(1);
+    *valore = m->vettore[index];
+
+    printf("Consumazione di tipo 2 (valore=%d, index=%d)\n", *valore, index);
+
+    m->stato[index]=LIBERO;
+    m->num_liberi++;
+    pthread_cond_signal(&(m->ok_prod_cv));
+    pthread_mutex_unlock(&(m->mutex));
+}
---
+++ My test/2021_17_05/2021_17_05/prodcons.h
@@ -0,0 +1,38 @@
+#ifndef __PROCEDURE_H
+#define __PROCEDURE_H
+
+#include <pthread.h>
+
+#define DIM 4
+
+#define LIBERO 0
+#define INUSO 1
+#define OCCUPATO1 2
+#define OCCUPATO2 3
+
+typedef struct{
+
+	int vettore[DIM];
+
+	int stato[DIM];
+
+	int num_liberi;
+	int num_occupati_tipo1;
+	int num_occupati_tipo2;
+
+	/* TBD: Aggiungere ulteriori variabili per la sincronizzazione */
+	pthread_mutex_t mutex;
+	pthread_cond_t ok_prod_cv;
+	pthread_cond_t ok_cons_cv;
+
+} MonitorPC;
+
+void inizializza(MonitorPC * m);
+void rimuovi(MonitorPC * m);
+void produci_tipo_1(MonitorPC * m, int valore);
+void produci_tipo_2(MonitorPC * m, int valore);
+void consuma_tipo_1(MonitorPC * m, int * valore);
+void consuma_tipo_2(MonitorPC * m, int * valore);
+
+
+#endif
